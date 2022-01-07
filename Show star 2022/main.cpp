3069a90f33a1f82bced139d56128e90e42055b02
Hosein Ghasemizade <hosein@ghasemizade.com>
2022-01-07 17:37:30 +0330

    show star 2022

---
+++ Show star 2022/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+//	define input
+	int num,i;
+	
+	cout <<"please enter the number of start :";
+//	get input
+	cin >> num;
+
+	
+//	define loop
+	for (i = 1; i <= num; i++)
+	{
+		cout<<'*';
+		
+	}
+	return 0;
+}
