ef89655aa362601a5f7307f05737b69c20e3e802
Hosein Ghasemizade <hosein@ghasemizade.com>
2022-01-07 14:43:33 +0330

    counter of char

---
+++ counter/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int count,str;
+	
+	cout<<"input the value then press enter :";
+	
+	while (cin.get() != '\n')
+		count++;
+		{
+			std::stringstream  stream(str);
+			std::string        oneWord;
+		cout<<"Number of your characters :"<<count;
+		}
+	
+	
+	return 0;
+}
