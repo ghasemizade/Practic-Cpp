99829d44a2cfa3c7dd8cbe1b435273d27cb022ad
Hosein Ghasemizade <hosein@ghasemizade.com>
2022-01-07 15:17:02 +0330

    X pow Y 2022

---
+++ X pow Y 2022/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Hosein Ghasemizade
+
+int main() {
+//	defined input
+	int x,y;
+	
+	cout<<"please enter the X :";
+//	take x
+	cin>>x;
+	cout<<"please enter the Y :";
+//	take y
+	cin>>y;
+//	Perform operations
+	cout<<pow(x,y);
+	
+	return 0;
+}
