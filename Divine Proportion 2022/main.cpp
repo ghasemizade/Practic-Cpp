69de726a00b4b6de0650d0942f753dd91d46ee00
Hosein Ghasemizade <hosein@ghasemizade.com>
2022-01-07 17:27:16 +0330

    Divine Proportion 2022

---
+++ Divine Proportion 2022/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+//	define input
+	int num;
+	
+	cout << "please enter the number :";
+//	get input
+	cin >> num;
+	
+
+	cout<<"The divine ratio of your input number :"<<num/1.618;
+	cout<<"The divine ratio of your input number :"<<num*1.618;
+	
+	
+	return 0;
+}
